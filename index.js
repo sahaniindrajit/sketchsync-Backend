fc98c89f856bb1b450819203a9014ef0f4108a85
Indrajit sahani <indrajit38mig@gmail.com>
2024-12-30 21:32:37 +0530

    Add Websocket connection

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import { Server } from 'socket.io';
 const port = process.env.PORT || 3000
 const app = express()
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    origin: 'http://localhost:5173/',
+    credentials: true,
+});
 
 app.use(express.json());
 app.use(morgan('tiny'))
@@ -16,28 +19,23 @@ app.use(cors({
     credentials: true,
 }));
 
-let whiteboards = {};
 
 io.on("connection", (socket) => {
     console.log("A user connected");
 
-    socket.on("joinRoom", (roomID) => {
-        socket.join(roomID);
-        console.log(`user joined on ${roomID}`)
-        socket.emit("whiteboardData", whiteboards[roomID])
-    })
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        console.log(`User ${socket.id} joined room: ${roomId}`);
+    });
 
-    socket.on("draw", (roomID, drawData) => {
-        if (!whiteboards[roomID]) {
-            whiteboards[roomID] = []
-        }
+    // Broadcast drawing data
+    socket.on('draw', (data) => {
+        socket.to(data.room).emit('draw', data);
+    });
 
-        whiteboards[roomID].push(drawData);
-        socket.to(boardId).emit("draw", drawData);
-    })
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
     });
 
 })
